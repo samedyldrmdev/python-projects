d1b57ebf3c38f3a529ec33f5255fc5c22b810d36
Samet Yıldırım <106842297+samettyldrm@users.noreply.github.com>
2022-07-02 02:44:25 +0300

    Update opensea-selenium.py
    Signed-off-by: Samet Yıldırım <106842297+samettyldrm@users.noreply.github.com>

--- opensea-selenium.py
+++ opensea-selenium.py
@@ -1,12 +1,42 @@
+from asyncio import selector_events
 from selenium import webdriver
 from time import sleep
-
+import requests
+from bs4 import BeautifulSoup
+import re
+import time
 
 browser = webdriver.Chrome()
 browser.maximize_window()
-browser.get("https://opensea.io/activity?search[eventTypes][0]=AUCTION_SUCCESSFUL&search[chains][0]=MATIC&search[chains][1]=ETHEREUM")
-sleep(5)
-selector = '#main > div > div > div > div.fresnel-container.fresnel-greaterThanOrEqual-xl.fill-remaining-height > div.sc-1xf18x6-0.sc-1twd32i-0.bdMwSd.kKpYwv > div.sc-1xf18x6-0.sc-1twd32i-0.sc-1wwz3hp-0.hRjaBd.kKpYwv.kuGBEl > div.sc-1xf18x6-0.cXZCEh > div > div:nth-child(2) > div:nth-child(19) > button > div > div:nth-child(6) > div > a'
-button = browser.find_element("css selector", selector)
-browser.execute_script("arguments[0].click();", button)
-sleep(10)
+url = "https://opensea.io/activity?search[eventTypes][0]=AUCTION_SUCCESSFUL&search[chains][0]=MATIC&search[chains][1]=ETHEREUM"
+browser.get(url)
+
+def Browser():
+
+    sleep(5)
+    selector = 'div.AccountLink--ellipsis-overflow > a'
+    # button = browser.find_element("css selector", selector)
+    username = browser.find_element("css selector", selector).text
+
+    def kullanici():
+        header = {"user-agent": "Mozilla/5.0"}
+        url = "https://opensea.io/" + username
+        content = requests.get(url, headers=header).content
+        soup = BeautifulSoup(content, "html.parser")
+        titles = soup.find_all("a", {"class": "sc-1pie21o-0 elyzfO"})
+        titles = str(titles)
+        twitter = re.findall("(https://twitter\.com/\S+)", titles)
+        instagram = re.findall("(https://instagram\.com/\S+)", titles)
+
+        print(username)
+        if twitter != []:
+            print(twitter)
+        if instagram != []:
+            print(instagram)
+        else:
+            print("Social media account not found!")
+    return kullanici()
+
+while True:
+    Browser()
+    sleep(10)
