8d73d8215714c7fee55fecc66c658367d7d9ff68
Samet Yıldırım <106842297+samettyldrm@users.noreply.github.com>
2022-07-02 05:20:40 +0300

    Update opensea-selenium.py
    Signed-off-by: Samet Yıldırım <106842297+samettyldrm@users.noreply.github.com>

--- opensea-selenium.py
+++ opensea-selenium.py
@@ -11,11 +11,12 @@ browser.maximize_window()
 url = "https://opensea.io/activity?search[eventTypes][0]=AUCTION_SUCCESSFUL&search[chains][0]=MATIC&search[chains][1]=ETHEREUM"
 browser.get(url)
 
-def Browser():
+twitter_accounts = []
+instagram_accounts = []
 
+def Browser():
     sleep(5)
-    selector = 'div.AccountLink--ellipsis-overflow > a'
-    # button = browser.find_element("css selector", selector)
+    selector = 'div > button > div > div:nth-child(6) > div'
     username = browser.find_element("css selector", selector).text
 
     def kullanici():
@@ -28,15 +29,31 @@ def Browser():
         twitter = re.findall("(https://twitter\.com/\S+)", titles)
         instagram = re.findall("(https://instagram\.com/\S+)", titles)
 
-        print(username)
+        print("username:", username)
         if twitter != []:
             print(twitter)
+            if twitter not in twitter_accounts:
+                twitter_accounts.append(twitter)
+                with open("sosyalmedyahesaplari.txt", "a+") as dosya:
+                    dosya_icerigi = dosya.readlines()
+                    for t in twitter_accounts:
+                        if t not in dosya_icerigi:
+                            dosya.write(str(t)+"\n")
         if instagram != []:
             print(instagram)
+            instagram_accounts.append(instagram)
+            if instagram not in instagram_accounts:
+                instagram_accounts.append(instagram)
+                with open("sosyalmedyahesaplari.txt", "a+") as dosya:
+                    dosya_icerigi = dosya.readlines()
+                    for i in instagram_accounts:
+                        if i not in dosya_icerigi:
+                            dosya.write(str(i)+"\n")
         else:
             print("Social media account not found!")
+
     return kullanici()
 
 while True:
     Browser()
-    sleep(10)
+    # sleep(5)
